c034d1a535d91fc5b4e28d25dc26fdb2ba465c86
DaveBrun94 <david.brunner@desy.de>
2019-12-11 13:40:42 +0100

    Dummy cms config

---
+++ crab/dummy.py
@@ -0,0 +1,7 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("Dummy")
+process.source = cms.Source("EmptySource")
+
+process.maxEvents = cms.untracked.PSet(
+  input = cms.untracked.int32(1),
+)
