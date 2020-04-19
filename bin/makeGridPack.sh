e68972a60a37e9a4a5eb4052be13dc2c76ff84ae
DaveBrun94 <david.brunner@desy.de>
2020-04-19 23:48:10 +0200

    Produce gridpack with official CMS repo

--- bin/makeGridPack.sh
+++ bin/makeGridPack.sh
@@ -1,42 +1,24 @@
 #!/bin/bash
 
-cd $1
-MHC=$2
-MH=$3
+mkdir -p gridpacks
 
-if [ ! -f "madCommands.txt" ]; then    
-    ##Write command for madgraph
-    echo -e "import model_v4 2HDMC" >> madCommands.txt
-    echo -e "define hc = h- h+" >> madCommands.txt
-    echo -e "define w = w+ w-" >> madCommands.txt
-    echo -e "define l = l+ l-" >> madCommands.txt
-    echo -e "define v = vl vl~" >> madCommands.txt
-    echo -e "generate p p > hc h1, h1 > b b~, (hc > h1 w, h1 > b b~, w > l v)" >> madCommands.txt
-    echo -e "add process p p > hc h1 j, h1 > b b~, (hc > h1 w, h1 > b b~, w > l v)" >> madCommands.txt
-    echo -e "add process p p > hc h1 j j, h1 > b b~, (hc > h1 w, h1 > b b~, w > l v)" >> madCommands.txt
-    echo -e "set automatic_html_opening False" >> madCommands.txt
-    echo -e "output HPlusAndH_ToWHH_ToL4B" >> madCommands.txt
-    echo -e "launch HPlusAndH_ToWHH_ToL4B" >> madCommands.txt
-    echo -e "${MCDIR}/ChargedProduction/SLHA/HPlusAndH_ToWHH_ToL4B_${MHC}_${MH}.shla" >> madCommands.txt
-    echo -e "set pdlabel lhapdf" >> madCommands.txt 
-    echo -e "set lhaid 306000" >> madCommands.txt
-    echo -e "set use_syst .true." >> madCommands.txt 
-    echo -e "set gridpack .true." >> madCommands.txt 
-    echo -e "done" >> madCommands.txt  
+unset CMSSW_VERSION CMSSW_BASE
+source /cvmfs/cms.cern.ch/cmsset_default.sh
 
-    ##Extract grid pack, compile and repack again
-    $MCDIR/MG5_aMC_v2_6_7/bin/mg5_aMC madCommands.txt
-    mv HPlusAndH_ToWHH_ToL4B/run_01_gridpack.tar.gz .
-    tar -xvf run_01_gridpack.tar.gz
-    mv run_01_gridpack.tar.gz gridpack_uncompiled.tar.gz
-    cd madevent/bin/
-    ./compile
-    ./clean4grid
-    cd ..
-    cd ..
-    tar -czvf gridpack.tar.gz madevent/ run.sh
-    command rm -rf madevent run.sh
+MHC=$1
+MH=$2
 
-else
-    echo "Gridpack is already produced"
-fi
+cd $MCDIR/genproductions/bin/MadGraph5_aMCatNLO/
+mkdir -p cards/signal/HPlusAndH_ToWHH_ToL4B_${MHC}_${MH}
+
+cp -f $MCDIR/ChargedProduction/cards/HPlusAndH_ToWHH_ToL4B_proc_card.dat cards/signal/HPlusAndH_ToWHH_ToL4B_${MHC}_${MH}/HPlusAndH_ToWHH_ToL4B_${MHC}_${MH}_proc_card.dat
+cp -f $MCDIR/ChargedProduction/cards/HPlusAndH_ToWHH_ToL4B_run_card.dat cards/signal/HPlusAndH_ToWHH_ToL4B_${MHC}_${MH}/HPlusAndH_ToWHH_ToL4B_${MHC}_${MH}_run_card.dat
+cp -f $MCDIR/ChargedProduction/SLHA/HPlusAndH_ToWHH_ToL4B_${MHC}_${MH}.shla cards/signal/HPlusAndH_ToWHH_ToL4B_${MHC}_${MH}/HPlusAndH_ToWHH_ToL4B_${MHC}_${MH}_param_card.dat
+
+echo "output HPlusAndH_ToWHH_ToL4B_${MHC}_${MH}" >> cards/signal/HPlusAndH_ToWHH_ToL4B_${MHC}_${MH}/HPlusAndH_ToWHH_ToL4B_${MHC}_${MH}_proc_card.dat
+
+./gridpack_generation.sh HPlusAndH_ToWHH_ToL4B_${MHC}_${MH} cards/signal/HPlusAndH_ToWHH_ToL4B_${MHC}_${MH}
+
+cd $MCDIR
+source $MCDIR/ChargedProduction/setenv.sh
+mv $MCDIR/genproductions/bin/MadGraph5_aMCatNLO/*tar* gridpacks
